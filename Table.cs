2c990866b61b2594d88867ddde813edfb2f1f228
nickchuchunov <nick.chuchunov@yandex.ru>
2022-03-18 22:25:52 +0300

    удаление лишних файлов

---
+++ Table.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageDriven_ArchitectureCNN_1.Task1
+{
+    public  class Table
+    {
+        public State State { get;    set; }
+
+        public int StatesCount { get; set; }
+
+        public int TableIndex { get; set; }
+
+
+        
+
+
+
+
+    }
+}
